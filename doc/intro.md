b42bf630f7ee9fae62783eb4eb789ea987cafc65
KingCode <jrompre@gmail.com>
2015-07-29 10:28:35 -0400

    clojure project with 'lein new', artifacts added

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fingerfood
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/fingerfood/core_test.clj
@@ -0,0 +1,7 @@
+(ns fingerfood.core-test
+  (:require [clojure.test :refer :all]
+            [fingerfood.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
