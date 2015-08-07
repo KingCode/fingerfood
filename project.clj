578ff8501a9c56b19f949757c10cb887ac043278
KingCode <jrompre@gmail.com>
2015-08-06 20:00:20 -0400

    clojure 1.6.0 due to slow repl

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-alpha2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]        
                  [jline "2.12.1"]
                  [org.clojure/core.match "0.3.0-alpha4"]])
